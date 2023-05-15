37d3cef95db2b9402c5923ca1dc030ccc9b78b37
Greg Tracy <gtracy@gmail.com>
2023-05-15 14:22:43 -0500

    implemented getstops method

--- src/transit-api.js
+++ src/transit-api.js
@@ -1,6 +1,6 @@
 
 const TRANSIT_API_ENDPOINT = "https://api.smsmybus.com/v1";
-const MOCK_ON_ERROR = true;
+const MOCK_ON_ERROR = false;
 const mock_route_data = [
     {routeID:'XX',minutes:5,destination:'WEST TRANSFER VIA SHERMAN'},
     {routeID:'02',minutes:8,destination:'WEST TRANSFER VIA SHERMAN'},
@@ -8,12 +8,19 @@ const mock_route_data = [
     {routeID:'04',minutes:12,destination:'SOUTH TRANSFER'},
     {routeID:'28',minutes:25,destination:'WEST TRANSFER'},
     {routeID:'15',minutes:46,destination:'HIGH POINT: VIA OLD MIDDLETON'}
+];
+const mock_stop_data = [
+    {stopID:'0100',intersection:'Universy & N. Park',lat:43.073399,lon:-89.401365,direction:'westbound'},
+    {stopID:'1100',intersection:'Miffling & Pinckney',lat:43.076028,lon:-89.384661,direction:'westbound'},
+    {stopID:'1505',intersection:'Jennifer & Ingersoll',lat:43.081151,lon:-89.365311,direction:'eastbound'},
+    {stopID:'1878',intersection:'Jennifer & Ingersoll',lat:43.081482,lon:-89.365249,direction:'westbound'},
 ]
 
 
 export default class TransitAPI {
     constructor(devkey) {
         this.devkey = devkey;
+        this.stop_details = undefined;//mock_stop_data;
     }
 
     getArrivals = async (stopid,routeid) => {
@@ -43,26 +50,37 @@ export default class TransitAPI {
 
     }
 
-    getStopLocation = async(stopid) => {
-        let endpoint = TRANSIT_API_ENDPOINT + "/getstoplocation?key=" + this.devkey + "&stopID=" + stopid;
+    getStops = async() => {
+        let endpoint = TRANSIT_API_ENDPOINT + "/getstops?key=" + this.devkey
 
-        fetch(endpoint)
-        .then(res => res.json())
-        .then(
-            (result) => {
+        if( this.stop_details ) {
+            return this.stop_details;
+        } else {
+            try {
+                const raw = await fetch(endpoint);
+                const result = await raw.json();
+    
                 if( result.status === "0" ) {
-                    return result.intersection;
+                    this.stop_details = result.stops;
+                    return result.stops;
                 } else {
-                    return undefined;
+                    console.error('API is returning an error. :(');
+                    console.dir(result);    
+                    return [];
                 }
-            },
-            (error) => {
-                console.log('Failed to fetch stop location');
-                console.dir(error);
-                return undefined;
+            } catch(e) {
+                if( MOCK_ON_ERROR ) {
+                    return mock_stop_data;
+                } else {
+                    console.log(e);
+                }
             }
-        )
-        .catch(console.log);
+    
+        }
+    }
+
+    getStopLocation = (stopid) => {
+        return {};
     }
 
     
