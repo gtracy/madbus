14cb24bc8586a5f3039ecef0df3fa3869d369038
Greg Tracy <gtracy@gmail.com>
2023-05-20 11:42:51 -0500

    added edit menu item (that is not hooked up yet)

--- src/components/StopList.js
+++ src/components/StopList.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { makeStyles } from '@mui/styles';
 
 import { Button, List, ListItem, ListItemText} from '@mui/material';
-import { Menu, MenuItem} from '@mui/material';
+import { Box, Menu, MenuItem} from '@mui/material';
 import { Typography } from '@mui/material';
 
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import PlaceOutlinedIcon from '@mui/icons-material/PlaceOutlined';
+import AddBoxRoundedIcon from '@mui/icons-material/AddBoxRounded';
+import EditLocationAltIcon from '@mui/icons-material/EditLocationAlt';
 
 import { useBookmarks } from '../bookmarks';
 
+const useStyles = makeStyles({
+    menuAction: {
+        padding: '0px 0px 0px 4px',
+        margin: '0px 0px 0px 4px',
+        minHeight: '12px'
+    },
+  });
+  
 
 export default function StopList({handleSelection}) {
     const { bookmarks } = useBookmarks();
     const navigate = useNavigate();
+    const classes = useStyles();
 
     const [anchorEl, setAnchorEl] = useState(null);
     const [selectedIndex, setSelectedIndex] = useState(0);
@@ -78,12 +90,18 @@ export default function StopList({handleSelection}) {
                 </MenuItem>
             ))}
             <hr/>
-            <Button
-                color="primary"
-                onClick={handleMenuAddClick}
-            >
-                + Add
-            </Button>
+            <MenuItem className={classes.menuAction}>
+                <Button onClick={handleMenuAddClick}>
+                    <AddBoxRoundedIcon/> 
+                    <Typography ml={1}>Add</Typography>
+                </Button>
+            </MenuItem>
+            <MenuItem className={classes.menuAction}>
+                <Button onClick={handleMenuAddClick}>
+                    <EditLocationAltIcon/>
+                    <Typography ml={1}>Edit</Typography>                
+                </Button>
+            </MenuItem>
         </Menu>
         </div>
     );
